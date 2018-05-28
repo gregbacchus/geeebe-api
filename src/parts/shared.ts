0263db08963507b5861dcb752d2a8edb65a3c193
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2018-05-28 19:52:39 +1200

    added parentIds

--- src/parts/shared.ts
+++ src/parts/shared.ts
@@ -2,11 +2,21 @@ import { StringChildValidator } from 'better-validator/src/IsObject';
 
 export namespace Shared {
 
-  export function checkAccessOneParams(params: StringChildValidator): void {
-    params('id').required().lengthInRange(1);
+  export function checkAccessOneParams(params: StringChildValidator, parentIds?: string[]): void {
+    if (parentIds) {
+      parentIds.forEach((id) => {
+        params(id).required().notEmpty();
+      });
+    }
+    params('id').required().notEmpty();
   }
 
-  export function checkAccessManyParams(params: StringChildValidator): void {
+  export function checkAccessManyParams(params: StringChildValidator, parentIds?: string[]): void {
+    if (parentIds) {
+      parentIds.forEach((id) => {
+        params(id).required().notEmpty();
+      });
+    }
     params().strict();
   }
 }
