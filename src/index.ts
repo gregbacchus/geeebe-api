28c1b69e511e4e89b586b1a222e12cf2b93e5131
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2021-06-14 18:21:02 +1200

    chore: exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 export * from './api';
 export * from './authorization';
+export * from './error';
+export * from './helmet';
+export * from './koa-service';
+export * from './middleware';
+export * from './monitoring-service';
+export * from './prometheus';
+
